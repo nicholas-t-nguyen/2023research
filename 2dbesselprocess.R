f00c6f4d860c5bc15b505214d27e6b31a8b70d5d
Nicholas Nguyen <nickngu24@gmail.com>
2023-11-26 03:40:19 -0700

    bessel process

---
+++ 2dbesselprocess.R
@@ -0,0 +1,11 @@
+N <- 1000;
+disx <- rnorm(N, 0, 1);
+disy <- rnorm(N, 0, 1);
+disz <- rnorm(N, 0, 1);
+cumdisx <- cumsum(disx);
+cumdisy <- cumsum(disy);
+cumdisz <- cumsum(disz);
+norm <- sqrt(cumdisx^2 + cumdisy^2 + cumdisz^2);
+plot(norm, type="l", main="Bessel process of 3d Wiener process", xlab="time", ylab="norm");
+
+
