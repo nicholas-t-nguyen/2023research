0d652c7e0d4b99084fdf7835a6512f606f047c04
Nicholas Nguyen <nickngu24@gmail.com>
2023-12-14 02:49:07 -0700

    sorting

--- diffeq.py
+++ diffeq.py
@@ -4,10 +4,8 @@ from scipy.integrate import solve_ivp
 
 def dh_dt(t, h, lambda1, lambda2):
     return (1 / 2) * (((-2) / (h - (lambda1(t)))) + ((-2) / (h + (lambda2(t)))))
-def solve_diffeq(t, steps, h0_values, lambda1, lambda2):
+def solve_diffeq(t, steps, h0, lambda1, lambda2):
     t_span = [0, t]
     t_eval = np.linspace(0, t, steps)
-    for h0 in h0_values:
-        sol = solve_ivp(dh_dt, t_span=t_span, y0=[h0], t_eval=t_eval, args=(lambda1, lambda2))
-        plt.plot(sol.t, sol.y[0], label=f'x={h0}')
+    sol = solve_ivp(dh_dt, t_span=t_span, y0=h0, t_eval=t_eval, args=(lambda1, lambda2))
     return sol
--- diffeq_run.py
+++ diffeq_run.py
@@ -4,17 +4,16 @@ from diffeq import solve_diffeq
 
 h0_values = [10]
 steps = 10000000
-t = 20
+t = 100
 
-lambda1 = lambda t: (100 * t ** 2)
-lambda2 = lambda t: (100 * t ** 2)
+lambda1 = lambda t: t
+lambda2 = lambda t: t
 
 plt.figure(figsize=(8, 6))
 
 if __name__ == '__main__':
     sol = solve_diffeq(t, steps, h0_values, lambda1, lambda2)
     min_value = np.min(sol.y[0])
-    print(f"Minimum value of the solution: {min_value}")
 
     plt.xlabel('t')
     plt.ylabel('h(t)')
