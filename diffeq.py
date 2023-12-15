4073c591cb433117ea4ca82cb90947044c573739
Nicholas Nguyen <nickngu24@gmail.com>
2023-12-15 03:13:05 -0700

    correction

--- diffeq.py
+++ diffeq.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 
@@ -7,5 +6,5 @@ def dh_dt(t, h, lambda1, lambda2):
 def solve_diffeq(t, steps, h0, lambda1, lambda2):
     t_span = [0, t]
     t_eval = np.linspace(0, t, steps)
-    sol = solve_ivp(dh_dt, t_span=t_span, y0=h0, t_eval=t_eval, args=(lambda1, lambda2))
+    sol = solve_ivp(dh_dt, t_span=t_span, y0=[h0], t_eval=t_eval, args=(lambda1, lambda2))
     return sol
