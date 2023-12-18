99f77ecec8dbd405425f2ab28194bbb686c3c957
Nicholas Nguyen <nickngu24@gmail.com>
2023-12-18 01:56:27 -0700

    adjustments

--- stochastic.py
+++ stochastic.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+def random_walk(t, steps):
+    dt = t / steps
+    step_set = [-dt, dt]
+    array = np.random.choice(step_set, size=steps)
+    array = np.concatenate([[0], array])
+    array = np.cumsum(array)
+    return array
+
 def brownian_motion(t, steps):
     dt = t / steps
     sd = np.sqrt(dt)
@@ -16,3 +24,6 @@ def brownian_motion_nd(dimensions, t, steps):
 
 def bessel_process(dimensions, t, steps):
     return np.linalg.norm(brownian_motion_nd(dimensions, t, steps), axis=0)
+
+if __name__ == '__main__':
+    print(random_walk(1, 10))
