b0b4f6927e4af2f2701492ea268285999220083d
nick <nickngu24@gmail.com>
2024-01-25 20:21:39 -0700

    fixing lambda

--- test scripts/test2.py
+++ test scripts/test2.py
@@ -7,14 +7,14 @@ from stochastic import brownian_motion
 from scipy.interpolate import interp1d
 from scipy.optimize import brentq as root
 
-t = 100
+t = 1
 steps = 1000
 ss = t/steps
 t_eval = np.arange(0, t+ss, ss)
 kappa = 1
-alpha = 1
+alpha = 10
 
-fun = lambda x: x
+fun = lambda x: 100 * x
 
 plt.figure(figsize=(8, 6))
 
@@ -23,25 +23,26 @@ y= []
 
 #bmv = brownian_motion(t, steps)
 #bm = interp1d(t_eval, bmv)
-for alpha in numpy.linspace(0, 10, 1001):
-    sol = solve_diffeq_scaled(t, steps, 10, kappa, fun, alpha)
-    solinterp = interp1d(t_eval, sol.y[0])
+#for alpha in numpy.linspace(0, 10, 1001):
+sol = solve_diffeq_scaled(t, steps, 10, kappa, fun, alpha)
+solinterp = interp1d(t_eval, sol.y[0])
 
-    lambdafun = lambda t, alpha=alpha: np.sqrt(kappa) * fun(t) + alpha * t
-    rootfun = lambda t, alpha=alpha: lambdafun(t) - solinterp(t)
+lambdafun = lambda t, alpha=alpha: np.sqrt(kappa) * fun(t) + alpha * t
+rootfun = lambda t, alpha=alpha: lambdafun(t) - solinterp(t)
 
-    fht = root(rootfun, a=0, b=50)
-    x.append(fht)
-    y.append(fun(fht))
+fht = root(rootfun, a=0, b=1)
+print(fht)
+x.append(fht)
+y.append(lambdafun(fht))
 
-    plt.show()
+plt.show()
 
-#plt.plot(sol.t, sol.y[0])
-#plt.plot(t_eval, [fun(t) for t in t_eval], color='brown')
+plt.plot(sol.t, sol.y[0])
+plt.plot(t_eval, [lambdafun(t) for t in t_eval], color='brown')
 plt.scatter(x, y, color='red')
 
 plt.xlabel('t')
 plt.ylabel('fht')
 
+plt.savefig("fht/100x.png")
 plt.show()
-#plt.savefig("fht/x.png")
